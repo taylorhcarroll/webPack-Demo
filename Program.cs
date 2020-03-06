b8e358a810f5748353907080ae1a16daac87f5a6
Adam <adam.e.sheaffer@gmail.com>
2020-03-06 10:08:22 -0600

    starting data

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nssStudents = new List<Student>()
+            {
+                new Student()
+                {
+                Name = "Spencer",
+                CohortName = "Day Cohort 37"
+                },
+
+                new Student()
+                {
+                Name = "Heidi",
+                CohortName = "Day Cohort 35"
+                },
+
+                new Student()
+                {
+                Name = "Namita",
+                CohortName = "Day Cohort 37"
+                },
+
+                new Student()
+                {
+                Name = "Holden",
+                CohortName = "Day Cohort 37"
+                },
+
+                new Student()
+                {
+                Name = "Aryn",
+                CohortName = "Day Cohort 35"
+                },
+
+                new Student()
+                {
+                Name = "Aryn",
+                CohortName = "Day Cohort 35"
+                },
+
+                new Student()
+                {
+                Name = "Jansen",
+                CohortName = "Day Cohort 37"
+                }
+            };
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,9 @@
+namespace LinqIntro
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string SlackHandle { get; set; }
+        public string CohortName { get; set; }
+    }
+}
